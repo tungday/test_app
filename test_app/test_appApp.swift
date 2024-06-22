53b7801d0ea659a0444b9039f8e8aa71b929868d
tungday <nguyenduytungptit1604vy@gmail.com>
2024-06-22 20:59:10 +0700

    Initial Commit

---
+++ test_app/test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_appApp.swift
+//  test_app
+//
+//  Created by Tung on 22/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
